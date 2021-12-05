b50df937e3b57fb59704a105136c9d2ba152c103
LeeCH <79134646+leechh0948@users.noreply.github.com>
2021-12-05 22:56:23 +0900

    Add PingPong (test)

--- sketch.js
+++ sketch.js
@@ -1,28 +1,46 @@
 let ball;
 let ping;
+let pong;
+let bg;
 
 function preload() {
   ping = loadImage('ping.png');
+  pong = loadImage('pong.png');
 }
 
 function setup(){
   createCanvas(600, 600);
   ball = new Ball();
+  bg = 15;
 }
 
 function draw(){
-  background(15);
-  image(ping, 0, 0);
+  addpingpong();
+  background(bg);
+
+  console.log(bg);
 
   let gravity = createVector(0, 0.1);
   ball.applyForce(gravity);
 
 
   ball.update();
-  ball.display();
+  ball.display(255);
   ball.edges();
 
 }
+
+function addpingpong(){
+  if (ball.pos.y >= 520 && bg == 15){
+    bg = 255;
+    image(pong, 0, 0);
+  }
+  else if (ball.pos.y >= 510 && bg == 255){
+    bg = 15;
+    image(ping, 0, 0);
+  }
+}
+
 
 class Ball{
   constructor(){
@@ -48,8 +66,8 @@ class Ball{
     }
   }
 
-  display(){
-    fill(255);
+  display(c){
+    fill(c);
     ellipse(this.pos.x, this.pos.y, this.r, this.r);
   }
 
